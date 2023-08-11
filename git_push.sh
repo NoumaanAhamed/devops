91fdaafd11dbd38a2a71d7592c193e7e18f3ad45
noumaan <noumaanahamed2@gmail.com>
2023-08-11 19:53:35 +0530

    .

--- git_push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Ask user for commit message
-read -p "Enter commit message: " commit_message
-
-# Execute git commands
-git add .
-git commit -m "$commit_message"
-git push origin main
